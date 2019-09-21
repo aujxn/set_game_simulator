6578080eee64c87a463c6b0e178d307306167b7f
aujxn <ajn6@pdx.edu>
2019-09-20 17:53:08 -0700

    changed data structures to record information about when class's of hands were incountered

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,38 @@ use itertools::Itertools;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use rayon::prelude::*;
-use std::ops::{Add, AddAssign};
+use std::ops::Add;
 
 /* Each card has 4 characteristics that can have 3 states */
 struct Card(usize, usize, usize, usize);
 
-/* Reports how many of each hand were encountered in the game */
+/* Reports how many of each hand were encountered in what part of the game */
 #[derive(Clone)]
-struct GameResult(i64, i64, i64, i64, i64, i64, i64);
+struct GameResult {
+    set12: i64,
+    set15: i64,
+    set18: i64,
+    set21: i64,
+    setless12: Vec<i64>,
+    setless15: Vec<i64>,
+    setless18: Vec<i64>,
+}
 
 impl Default for GameResult {
+    /* The first vec is all the 12's, second is 15's, third is 18's, and last is 21's
+     * The tuples inside the vecs are the setless and with set counts
+     * The index of the tuples is the number of remaining cards in the deck divided by 3
+     */
     fn default() -> Self {
-        GameResult(0, 0, 0, 0, 0, 0, 0)
+        GameResult {
+            set12: 0,
+            set15: 0,
+            set18: 0,
+            set21: 0,
+            setless12: vec![0; 24],
+            setless15: vec![0; 24],
+            setless18: vec![0; 24],
+        }
     }
 }
 
@@ -27,18 +47,33 @@ impl Add for GameResult {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
-        Self(
-            self.0 + other.0,
-            self.1 + other.1,
-            self.2 + other.2,
-            self.3 + other.3,
-            self.4 + other.4,
-            self.5 + other.5,
-            self.6 + other.6,
-        )
+        Self {
+            set12: self.set12 + other.set12,
+            set15: self.set15 + other.set15,
+            set18: self.set18 + other.set18,
+            set21: self.set21 + other.set21,
+            setless12: self
+                .setless12
+                .iter()
+                .zip(other.setless12.iter())
+                .map(|x| x.0 + x.1)
+                .collect(),
+            setless15: self
+                .setless15
+                .iter()
+                .zip(other.setless15.iter())
+                .map(|x| x.0 + x.1)
+                .collect(),
+            setless18: self
+                .setless18
+                .iter()
+                .zip(other.setless18.iter())
+                .map(|x| x.0 + x.1)
+                .collect(),
+        }
     }
 }
-
+/*
 impl AddAssign for GameResult {
     fn add_assign(&mut self, other: Self) {
         self.0 = self.0 + other.0;
@@ -50,7 +85,7 @@ impl AddAssign for GameResult {
         self.6 = self.6 + other.6;
     }
 }
-
+*/
 /* Reports findings of a single hand */
 enum Set {
     /* First 3 are the indices of the cards in the hand */
@@ -105,7 +140,7 @@ fn is_set(first: &Card, second: &Card, third: &Card) -> bool {
 
 /* Plays an entire game of set and returns some information */
 fn play_game() -> GameResult {
-    let mut result = GameResult(0, 0, 0, 0, 0, 0, 0);
+    let mut result = GameResult::default();
 
     /* This is the data structure for the full deck */
     let mut deck: Vec<Card> = vec![];
@@ -151,11 +186,11 @@ fn play_game() -> GameResult {
                                 None => return result,
                             }
                         }
-                        result.1 += 1;
+                        result.set12 += 1;
                     }
-                    15 => result.3 += 1,
-                    18 => result.5 += 1,
-                    21 => result.6 += 1,
+                    15 => result.set15 += 1,
+                    18 => result.set18 += 1,
+                    21 => result.set21 += 1,
                     _ => {
                         println!("Unreachable hand size: {:?}", hand.len());
                         unreachable!();
@@ -177,9 +212,9 @@ fn play_game() -> GameResult {
             Set::NotFound() => {
                 /* update the count of hand with no sets */
                 match hand.len() {
-                    12 => result.0 += 1,
-                    15 => result.2 += 1,
-                    18 => result.4 += 1,
+                    12 => result.setless12[deck.len() / 3] += 1,
+                    15 => result.setless15[deck.len() / 3] += 1,
+                    18 => result.setless18[deck.len() / 3] += 1,
                     _ => {
                         println!("Unreachable hand size: {:?}", hand.len());
                         unreachable!();
@@ -216,30 +251,38 @@ fn main() {
     /* Report the findings about the games */
     log::info!("After {:?} games of simulated... \n\n", games);
 
-    log::info!("12 card hands with no sets: {:?}", results.0);
-    log::info!("12 card hands where set was found: {:?}", results.1);
+    let setless12: i64 = results.setless12.iter().sum();
+    let setless15: i64 = results.setless15.iter().sum();
+    let setless18: i64 = results.setless18.iter().sum();
+
+    log::info!("12 card hands with no sets: {:?}", setless12);
+    log::info!("12 card hands where set was found: {:?}", results.set12);
     log::info!(
         "proportion of 12s w/out sets: {:.3}%\n",
-        100.0 * results.0 as f64 / results.1 as f64
+        100.0 * setless12 as f64 / results.set12 as f64
     );
 
-    log::info!("15 card hands with no sets: {:?}", results.2);
-    log::info!("15 card hands where set was found: {:?}", results.3);
+    log::info!("15 card hands with no sets: {:?}", setless15);
+    log::info!("15 card hands where set was found: {:?}", results.set15);
     log::info!(
         "proportion of 15s w/out sets: {:.3}%\n",
-        100.0 * results.2 as f64 / results.3 as f64
+        100.0 * setless15 as f64 / results.set15 as f64
     );
 
-    log::info!("18 card hands with no sets: {:?}", results.4);
-    log::info!("18 card hands where set was found: {:?}", results.5);
+    log::info!("18 card hands with no sets: {:?}", setless18);
+    log::info!("18 card hands where set was found: {:?}", results.set18);
     log::info!(
         "proportion of 18s w/out sets: {:.3}%\n",
-        100.0 * results.4 as f64 / results.5 as f64
+        100.0 * setless18 as f64 / results.set18 as f64
     );
 
     log::info!(
         "21 cards hands encountered: {:?}\n ({:?} games per 21 card hand)",
-        results.6,
-        games as i64 / results.6
+        results.set21,
+        if results.set21 != 0 {
+            games as i64 / results.set21
+        } else {
+            0
+        }
     );
 }
